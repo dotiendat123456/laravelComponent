270bff14acfd05955d34a540a336c877acf827da
DO TIEN DAT <dotiendst@gmail.com>
2025-07-22 10:50:27 +0700

    Chỉnh sửa lại helper

--- app/Helpers/SlugHelper.php
+++ app/Helpers/SlugHelper.php
@@ -4,13 +4,7 @@
 
 use Illuminate\Support\Str;
 
-// class SlugHelper
-// {
-//     public static function generateSlug(string $title, int $id): string
-//     {
-//         return Str::slug($title) . '-' . substr(md5($id), 0, 6);
-//     }
-// }
+
 
 
 if (!function_exists('generateSlug')) {
