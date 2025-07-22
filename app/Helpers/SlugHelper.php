f17a96984847613673e48953d648ba45061e8c9a
DO TIEN DAT <dotiendst@gmail.com>
2025-07-22 10:54:58 +0700

    Cập nhật lại file ảnh

--- app/Helpers/SlugHelper.php
+++ app/Helpers/SlugHelper.php
@@ -5,19 +5,9 @@
 use Illuminate\Support\Str;
 
 
-
-
 if (!function_exists('generateSlug')) {
-    /**
-     * Sinh slug kèm mã hash từ id để tránh trùng slug.
-     *
-     * @param string $title
-     * @param int $id
-     * @return string
-     */
     function generateSlug(string $title, int $id): string
     {
         return Str::slug($title) . '-' . substr(md5($id), 0, 6);
     }
 }
-
